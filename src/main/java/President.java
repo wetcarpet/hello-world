515052ce9d45cfccda062fa4e3827f3ceade5a16
wetcarpet <65464973+wetcarpet@users.noreply.github.com>
2020-10-04 22:09:15 +0300

    third line in github

--- src/main/java/President.java
+++ src/main/java/President.java
@@ -2,5 +2,6 @@ public class President {
     public static void main(String args[]){
         System.out.println("I like to rule");
         System.out.println("second line");
+        System.out.println("third line in github");
     }
 }
